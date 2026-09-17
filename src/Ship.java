45860175000652e42bacd45d39c9c3bb0e14c8df
deshawn <Deshawnjohal@gmail.com>
2026-09-17 15:52:24 +0100

    creates the abstract ship and square classes

---
+++ src/Ship.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Ship {
+    private String name;
+    private int length;
+    private int hits;
+    private ArrayList<Square> position;
+
+    public Ship(String name, int length){
+        this.name = name;
+        this.length = length;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getLength(){
+        return length;
+    }
+
+    public int getHits(){
+        return hits;
+    }
+
+    public ArrayList<Square> getPosition(){
+        return position;
+    }
+
+
+
+
+
+
+
+}
